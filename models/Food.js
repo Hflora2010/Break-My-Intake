47f382a72247d5d840ef4cc373679fc7ea416499
Hailey Flora <Haileycflora@gmail.com>
2023-05-01 19:47:15 -0400

    group adds model index and models, update server.js, add controller file

---
+++ models/Food.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Food extends Model {}
+
+    Food.init(
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            food_name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            calories: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            serving_size: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            },
+            quanity: {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            },
+        },
+    );
