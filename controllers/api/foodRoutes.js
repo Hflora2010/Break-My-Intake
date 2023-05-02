d4c6c0f7aeac12cd81503f996dedc0fef062d1d3
Josh Mol <joshua.molyneaux@live.com>
2023-05-01 21:10:34 -0400

    Adding updates to models, routes, and connection

---
+++ controllers/api/foodRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const { User, Food } = require('../../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, async (req,res)=> {
+
+    try {
+        const userData = await User.findAll({
+    
+            user_id: req.session.user_id,
+        });
+
+        //serialize 
+        const userResults = userData.map((data) => data.get({ plain: true}));
+
+        res.render('dashboard', {
+            userResults,
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+
+
--- models/Food.js
+++ models/Food.js
@@ -1,31 +1,40 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
-class Food extends Model {}
+class Food extends Model { }
 
-    Food.init(
-        {
-            id: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                primaryKey: true,
-                autoIncrement: true,
-            },
-            food_name: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            calories: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-            },
-            serving_size: {
-                type: DataTypes.INTEGER,
-                allowNull: false
-            },
-            quanity: {
-                type: DataTypes.INTEGER,
-                allowNull: false
-            },
+Food.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
         },
-    );
+        food_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        calories: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        serving_size: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        quanity: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "food",
+    }
+);
+
+module.exports = Food;
